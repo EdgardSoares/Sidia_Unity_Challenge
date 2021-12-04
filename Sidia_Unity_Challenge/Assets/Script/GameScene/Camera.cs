6820d728045a792bf3025f7ecf5855acd2a6bcbb
DESKTOP-QNMHJ5O\Edd <edgardsoares.ed@gmail.com>
2021-12-04 00:57:17 -0400

    Add CameraController to follow the player Project Update

---
+++ Sidia_Unity_Challenge/Assets/Script/GameScene/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 distanceCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanceCam = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + distanceCam;   
+    }
+}
--- Sidia_Unity_Challenge/Assets/Script/GameScene/CharacterGame.cs
+++ Sidia_Unity_Challenge/Assets/Script/GameScene/CharacterGame.cs
@@ -6,16 +6,28 @@ public class CharacterGame : MonoBehaviour
 {
     public GameObject[] characterPrefabs;
     public Transform spawn;
+    public GameObject prefab;
+    public GameObject novo;
+
+    public GameObject cam;
+    Vector3 distanceCam;
 
     // Start is called before the first frame update
     void Start()
     {
         int selectCharacter = PlayerPrefs.GetInt("selectCharacter"); //Vai pegar o index do personagem selecionado
-        GameObject prefab = characterPrefabs[selectCharacter]; //Selecionar o prefab
-        GameObject novo = Instantiate(prefab, spawn.position, Quaternion.identity); //Spawnar no mapa
+        prefab = characterPrefabs[selectCharacter]; //Selecionar o prefab
+        novo = Instantiate(prefab, spawn.position, Quaternion.identity); //Spawnar no mapa
 
         //Fazer o Spawn ser random no mapa
+
+        distanceCam = transform.position - novo.transform.position;
     }
 
-    
+    void Update()
+    {
+        transform.position = novo.transform.position + distanceCam;
+    }
+
+
 }
