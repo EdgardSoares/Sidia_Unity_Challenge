283ba018731d6b9d918eb08df5f54caee770edfa
DESKTOP-QNMHJ5O\Edd <edgardsoares.ed@gmail.com>
2021-12-03 23:24:33 -0400

    Add em Select Character a troca de personagens

--- Sidia_Unity_Challenge/Assets/Script/SelectChars/SelectCharacter.cs
+++ Sidia_Unity_Challenge/Assets/Script/SelectChars/SelectCharacter.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SelectCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject[] characters;
+    public int selectCharacter = 0;
+    public GameObject vikingText;
+    public GameObject knightText;
+
+
     void Start()
     {
-        
+        vikingText.SetActive(false);
+        characters[1].SetActive(false);
+
+    }
+    public void Next()
+    {
+        characters[selectCharacter].SetActive(false);
+        vikingText.SetActive(false);
+        selectCharacter = (selectCharacter + 1) % characters.Length;
+        characters[selectCharacter].SetActive(true);
+        vikingText.SetActive(true);
+        knightText.SetActive(false);
+        Debug.Log("Selecionou o Knight");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Previous()
     {
-        
+        vikingText.SetActive(false);
+        knightText.SetActive(true);
+        characters[selectCharacter].SetActive(false);
+        selectCharacter--;
+        if (selectCharacter < 0)
+        {
+            selectCharacter += characters.Length;
+        }
+        characters[selectCharacter].SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        PlayerPrefs.SetInt("selectCharacter", selectCharacter);
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
     }
 }
